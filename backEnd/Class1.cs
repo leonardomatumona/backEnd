6c10fd16fe4e8ce05e95c48c2cd9deefc5d8bedf
leonardomatumona <leonardomatumona@gmail.com>
2020-07-15 20:12:30 -0300

    Add project files.

---
+++ backEnd/Class1.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace backEnd
+{
+     public class Evento
+    {
+        public int id { get; set; }
+
+        public DateTime data { get; set; }
+
+        public string Titulo { get; set; }
+    }
+}
---
+++ backEnd/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace backEnd
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine();
+           
+        }
+    }
+}
