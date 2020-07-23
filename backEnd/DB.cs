79dc9cbaecb6c241239b71c2fd1401b3ba0b0fd0
leonardomatumona <leonardomatumona@gmail.com>
2020-07-22 21:28:31 -0300

    atualizaçao

---
+++ backEnd/DB.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Text.Json;
+
+namespace backEnd
+{
+    public class DB
+    {
+        public static IEnumerable<Evento> LerEventoDoArquivo()
+        {
+            var conteudoArquivo = System.IO.File.ReadAllText("data.json");
+
+            var lista = JsonSerializer.Deserialize<IEnumerable<Evento>>(conteudoArquivo);
+
+            return lista;
+        }
+        public static void SalvarArquivoNovo(IEnumerable<Evento> novaLista)
+        {
+            var json = System.Text.Json.JsonSerializer.Serialize(novaLista);
+            System.IO.File.WriteAllText("data.json", json);
+        }
+
+
+
+
+    }
+}
